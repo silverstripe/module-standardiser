0a8fe19e8e7676b3dabd815399debef9e07341b8
Steve Boyd <emteknetnz@gmail.com>
2023-08-21 14:58:19 +1200

    NEW Add auto-tag

---
+++ scripts/cms-any/auto-tag.php
@@ -0,0 +1,21 @@
+<?php
+
+$contents = <<<EOT
+name: Auto-tag
+on:
+  push:
+    tags:
+      - '*.*.*'
+  workflow_dispatch:
+jobs:
+  auto-tag:
+    name: Auto-tag
+    runs-on: ubuntu-latest
+    steps:
+      - name: Auto-tag
+        uses: silverstripe/gha-auto-tag@v1
+EOT;
+
+if (is_gha_repository()) {
+    write_file_even_if_exists(".github/workflows/auto-tag.yml", $contents);
+}
