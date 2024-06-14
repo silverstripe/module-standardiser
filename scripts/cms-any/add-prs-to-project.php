c8b89ea9b5d2e90b762d9365ae5d60361e4367f0
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2024-06-14 12:17:27 +1200

    NEW Add script for new action that adds PRs to project (#59)

---
+++ scripts/cms-any/add-prs-to-project.php
@@ -0,0 +1,31 @@
+<?php
+
+$content = <<<EOT
+name: Add new pull requests to a github project
+
+on:
+  pull_request:
+    types:
+      - opened
+      - ready_for_review
+
+permissions: {}
+
+jobs:
+  addprtoproject:
+    # Only run on the silverstripe account
+    if: github.repository_owner == 'silverstripe'
+    runs-on: ubuntu-latest
+    steps:
+      - name: Add PR to github project
+        uses: silverstripe/add-pr-to-project@v1
+EOT;
+
+$actionPath = '.github/workflows/add-prs-to-project.yml';
+$shouldHaveAction = module_account() === 'silverstripe' && is_module() && !module_is_recipe();
+
+if ($shouldHaveAction) {
+    write_file_even_if_exists($actionPath, $content);
+} else {
+    delete_file_if_exists($actionPath);
+}
